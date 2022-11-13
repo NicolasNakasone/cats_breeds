00d342ab520ca97b2483c6fc592f3fa6d59b7a96
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2022-11-13 20:46:52 -0300

    added test route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
+import { Box, Link } from "@mui/material";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -36,7 +36,15 @@ function App() {
           </div>
         }
       />
-      <Route path="test" element={<div>a</div>} />
+      <Route
+        path="test"
+        element={
+          <div>
+            <Box>a</Box>
+            <Link href={"/"}>to home</Link>
+          </div>
+        }
+      />
     </Routes>
   );
 }
